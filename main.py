d1ec5332bc3b0b9c80910c8b7bbb1cd961b8f50d
hashkiller <116666377+hashkiller@users.noreply.github.com>
2023-11-27 21:04:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 os.system('net stop qengine')
 os.system('net stop qupdate')
 
-print('------------------------------- \n Qustudio was killed (use this program everytime your computer is up)')
+print('------------------------------- \n Qustudio was killed (qustudio was killer only for this session)')
 time.sleep(5)
 print('exit...')
 os.system("exit")
